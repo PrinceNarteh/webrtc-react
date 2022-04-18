6f7ec9d1250d12ba625370ff54f774178ab358b1
Prince Narteh <prinarteh@gmail.com>
2022-04-18 11:51:48 +0000

    feat: configure server with socket.io

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("It's working now");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", () => {
+  console.log("User is connected");
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
